1287b95354475628c0a88769ce37dcc3f4f8ac78
BlackFuffey <ethanthecomputerman@gmail.com>
2024-06-12 07:17:07 -0400

    init

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- setup lazy and plugins
+require("lazy.lazy")
+
+-- vim script configs
+vim.cmd([[
+	" Set fallback background color
+	highlight Normal ctermbg=black
+	set background=dark
+
+	" Set theme
+	colorscheme catppuccin-mocha
+
+	" Enable line number
+	set number
+	set relativenumber
+
+	" Enable auto indent
+	set autoindent
+	filetype plugin indent on
+	set expandtab
+	set shiftwidth=2
+
+	" Syntax lighlighting
+	syntax on
+
+	" Use system clipboard
+	set clipboard=unnamedplus
+]])
