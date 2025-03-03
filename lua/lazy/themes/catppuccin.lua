286ca395a97269647b0bbfb03e66c14c33b2ccdb
BlackFuffey <blackfuffey@invalid>
2025-03-03 07:53:54 -0500

    Added extra stuff

--- lua/lazy/themes/catppuccin.lua
+++ lua/lazy/themes/catppuccin.lua
@@ -1,5 +1,17 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function ()
+        require("catppuccin").setup({
+            flavour = "latte",
+            transparent_background = true,
+            show_end_of_buffer = true,
+            dim_inactive = {
+                enabled = false,
+                shade = "dark",
+                percentage = 0.15
+            }
+        })
+    end
 }
